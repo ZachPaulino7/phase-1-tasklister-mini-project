93e232c705e34f1e2c16bb141d4c1fffc7b91126
Zachpaulino7 <Zachp7@live.com>
2022-08-03 16:56:04 -0500

    Completed assignment without stretch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const todoForm = document.getElementById('create-task-form');
+  todoForm.addEventListener('submit', handleSubmit);
 });
+
+function handleSubmit(event) {
+  event.preventDefault();
+ 
+
+const taskList = document.getElementById('tasks');
+  const task = document.createElement('li');
+  task.textContent = event.target['new-task-description'].value;
+  taskList.append(task);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+    task.remove();
+  });
+  task.append(deleteButton);
+
+  event.target.reset();
+}
